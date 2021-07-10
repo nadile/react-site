d73e9fc30e76c04bbad78cc59b2f2ab664e155fe
nadile <nadilerojda94@gmail.com>
2021-07-10 17:41:46 +0300

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection/index";
+import Sidebar from "../components/Sidebar";
+import InfoSection from "../components/InfoSection";
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from "../components/InfoSection/Data";
+
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+};
+
+export default Home;
